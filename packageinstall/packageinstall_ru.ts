acf58f5a9c778c8606a60a620c49822ea77f2519
Andrey Cherepanov <cas@altlinux.ru>
2011-07-26 01:12:02 +0400

    Update Russian translation of packageinstall-1.1.0

--- packageinstall/packageinstall_ru.ts
+++ packageinstall/packageinstall_ru.ts
@@ -4,80 +4,86 @@
 <context>
     <name>AptCommit</name>
     <message>
-        <location filename="aptcommit.cpp" line="78"/>
-        <source>There are %1 upgraded, %2 installed,
-%3 removed, %4 kept packages.
-Do you want to install this packages?</source>
-        <translation>Обновляемых пакетов: %1, новых: %2 
-удаляемых: %3, оставленных без изменений: %4
-Установить эти пакеты?</translation>
+        <location filename="aptcommit.cpp" line="79"/>
+        <source>&lt;p&gt;&lt;b&gt;Processed packages:&lt;/b&gt;&lt;/p&gt;
+</source>
+        <translation>&lt;p&gt;&lt;b&gt;Обрабатываемые пакеты:&lt;/b&gt;&lt;/p&gt;</translation>
     </message>
     <message>
-        <location filename="aptcommit.cpp" line="92"/>
-        <source>Packages to be installed:
-</source>
-        <translation>Пакеты, которые будут установлены:
-</translation>
+        <location filename="aptcommit.cpp" line="81"/>
+        <source>&amp;#9658; upgraded: %1&lt;br&gt;</source>
+        <translation>&amp;#9658; будет обновлено: %1&lt;br&gt;</translation>
+    </message>
+    <message>
+        <location filename="aptcommit.cpp" line="82"/>
+        <source>&amp;#9658; installed: %1&lt;br&gt;</source>
+        <translation>&amp;#9658; будет устанавлено: %1&lt;br&gt;</translation>
+    </message>
+    <message>
+        <location filename="aptcommit.cpp" line="83"/>
+        <source>&amp;#9658; removed: %1&lt;br&gt;</source>
+        <translation>&amp;#9658; будет удалено: %1&lt;br&gt;</translation>
+    </message>
+    <message>
+        <location filename="aptcommit.cpp" line="84"/>
+        <source>&amp;#9658; kept: %1&lt;br&gt;</source>
+        <translation>&amp;#9658; будет оставлено без изменений: %1&lt;br&gt;</translation>
+    </message>
+    <message>
+        <location filename="aptcommit.cpp" line="86"/>
+        <source>&lt;br&gt;Do you want to install this packages?</source>
+        <translation>&lt;br&gt;Установить эти пакеты?</translation>
     </message>
     <message>
         <location filename="aptcommit.cpp" line="96"/>
-        <source>
+        <source>&lt;p&gt;&lt;b&gt;Packages to be installed:&lt;/b&gt;&lt;/p&gt;</source>
+        <oldsource>
 Packages to be upgraded:
-</source>
-        <translation>Пакеты, которые будут обновлены:
+</oldsource>
+        <translation>&lt;p&gt;&lt;b&gt;Пакеты, которые будут обновлены:&lt;/b&gt;&lt;/p&gt;
 </translation>
     </message>
     <message>
         <location filename="aptcommit.cpp" line="100"/>
-        <source>
+        <source>&lt;p&gt;&lt;b&gt;Packages to be upgraded:&lt;/b&gt;&lt;/p&gt;</source>
+        <oldsource>
 Packages to be removed:
-</source>
-        <translation>Пакеты, которые будут удалены:
+</oldsource>
+        <translation>&lt;p&gt;&lt;b&gt;Пакеты, которые будут удалены:&lt;/b&gt;&lt;/p&gt;
 </translation>
     </message>
     <message>
         <location filename="aptcommit.cpp" line="104"/>
-        <source>
+        <source>&lt;p&gt;&lt;b&gt;Packages to be removed:&lt;/b&gt;&lt;/p&gt;</source>
+        <oldsource>
 Packages to be kept back:
-</source>
-        <translation>Пакеты, которые будут оставлены:
+</oldsource>
+        <translation>&lt;p&gt;&lt;b&gt;Пакеты, которые будут оставлены:&lt;/b&gt;&lt;/p&gt;
 </translation>
     </message>
     <message>
-        <location filename="aptcommit.cpp" line="114"/>
-        <source>Nothing to install</source>
-        <translation>Нечего обновлять</translation>
-    </message>
-    <message>
-        <location filename="aptcommit.cpp" line="114"/>
-        <source>Nothing to install.
-There are newest version of packages.</source>
-        <translation>Нет пакетов для установки.
-Установлены самое новое программное обеспечение.</translation>
+        <location filename="aptcommit.cpp" line="108"/>
+        <source>&lt;p&gt;&lt;b&gt;Packages to be kept back:&lt;/b&gt;&lt;/p&gt;</source>
+        <translation>&lt;p&gt;&lt;b&gt;Пакеты, которые будут оставлены без изменений:&lt;/b&gt;&lt;/p&gt;</translation>
     </message>
     <message>
         <location filename="aptcommit.cpp" line="118"/>
-        <source>Statistics</source>
-        <translation>Статистика</translation>
+        <source>&lt;b&gt;Nothing to install.&lt;/b&gt;&lt;br&gt;There are newest version of packages.</source>
+        <translation>&lt;b&gt;Нечего устанавливать.&lt;/b&gt;&lt;br&gt;Установлены последние версии пакетов.</translation>
     </message>
     <message>
-        <location filename="aptcommit.cpp" line="137"/>
-        <location filename="aptcommit.cpp" line="181"/>
+        <location filename="aptcommit.cpp" line="130"/>
+        <location filename="aptcommit.cpp" line="174"/>
         <source>Preparing...</source>
         <translation>Подготовка к установке...</translation>
     </message>
     <message>
-        <location filename="aptcommit.cpp" line="146"/>
+        <location filename="aptcommit.cpp" line="139"/>
         <source>Installation is finished successful</source>
         <translation>Установка завершена успешно</translation>
     </message>
     <message>
-        <location filename="aptcommit.cpp" line="184"/>
-        <source>Finishing installation...</source>
-        <translation>Завершение установки...</translation>
-    </message>
-    <message>
-        <location filename="aptcommit.cpp" line="186"/>
+        <location filename="aptcommit.cpp" line="176"/>
         <source>Installing package...</source>
         <translation>Установка пакета...</translation>
     </message>
@@ -85,109 +91,145 @@ There are newest version of packages.</source>
 <context>
     <name>Dialog</name>
     <message>
-        <location filename="dialog.cpp" line="45"/>
-        <source>Installing packages</source>
-        <translation>Установка пакетов</translation>
-    </message>
-    <message>
-        <location filename="dialog.cpp" line="57"/>
+        <location filename="dialog.cpp" line="48"/>
         <source>&lt;b&gt;Updating system&lt;/b&gt;&lt;br&gt;Please, wait...</source>
         <translation>&lt;b&gt;Обновление системы&lt;/b&gt;&lt;br&gt;Пожалуйста, подождите...</translation>
     </message>
     <message>
-        <location filename="dialog.cpp" line="58"/>
+        <location filename="dialog.cpp" line="49"/>
         <source>&lt;b&gt;Installing packages&lt;/b&gt;&lt;br&gt;Please, wait...</source>
         <translation>&lt;b&gt;Установка пакетов&lt;/b&gt;&lt;br&gt;Пожалуйста, подождите...</translation>
     </message>
     <message>
-        <location filename="dialog.cpp" line="74"/>
-        <source>&amp;Cancel</source>
-        <translation> &amp;Отмена</translation>
-    </message>
-    <message>
-        <location filename="dialog.cpp" line="75"/>
-        <source>Quit from application</source>
-        <translation>Выход из приложения</translation>
-    </message>
-    <message>
-        <location filename="dialog.cpp" line="171"/>
+        <location filename="dialog.cpp" line="140"/>
         <source>Error start</source>
         <translation>Ошибка запуска</translation>
     </message>
     <message>
-        <location filename="dialog.cpp" line="171"/>
-        <source>Could not start &apos;apt-get&apos;. Install this program.</source>
-        <translation>Невозможно запустить программу «apt-get».
-Проверьте установку этой программы.</translation>
+        <location filename="dialog.cpp" line="140"/>
+        <source>Could not start &apos;apt-get&apos; programm.
+Install this program.</source>
+        <translation>Не удалось запустить программу &apos;apt-get&apos;.
+Установите эту программу.</translation>
     </message>
     <message>
-        <location filename="dialog.cpp" line="176"/>
+        <location filename="dialog.cpp" line="145"/>
         <source>Check package requirements...</source>
         <translation>Проверка зависимостей пакетов...</translation>
     </message>
     <message>
-        <location filename="dialog.cpp" line="184"/>
+        <location filename="dialog.cpp" line="155"/>
         <source>&amp;Exit</source>
         <translation>В&amp;ыход</translation>
     </message>
     <message>
-        <location filename="dialog.cpp" line="220"/>
+        <location filename="dialog.cpp" line="206"/>
         <source>Unsufficient privileges</source>
         <translation>Недостаточно привилегий</translation>
     </message>
     <message>
-        <location filename="dialog.cpp" line="221"/>
+        <location filename="dialog.cpp" line="207"/>
         <source>Program should be run with superuser privileges.
 Check your rights and program installation.</source>
         <translation>Программа должна быть запущена с привилегиями
 суперпользователя (root). Проверьте права доступа.</translation>
     </message>
     <message>
-        <location filename="dialog.cpp" line="225"/>
+        <location filename="dialog.cpp" line="211"/>
         <source>Installation error</source>
         <translation>Ошибка установки</translation>
     </message>
     <message>
-        <location filename="dialog.cpp" line="226"/>
+        <location filename="dialog.cpp" line="212"/>
         <source>Installation is failed.
 Check output log.</source>
         <translation>Во время установки произошла ошибка.
 Работа приложения будет завершена.</translation>
     </message>
     <message>
-        <location filename="dialog.cpp" line="230"/>
+        <location filename="dialog.cpp" line="216"/>
         <source>Wrong package name</source>
         <translation>Невереное имя пакета</translation>
     </message>
     <message>
-        <location filename="dialog.cpp" line="231"/>
+        <location filename="dialog.cpp" line="217"/>
         <source>Couldn&apos;t find package
 &apos;%1&apos;</source>
         <translation>Невозможно найти пакет
 «%1»</translation>
     </message>
+    <message>
+        <location filename="dialog.cpp" line="229"/>
+        <location filename="dialog.cpp" line="237"/>
+        <source>Hide &amp;details &lt;&lt;</source>
+        <translation>Скрыть &amp;подробности &gt;&gt;</translation>
+    </message>
+    <message>
+        <location filename="dialog.cpp" line="232"/>
+        <location filename="dialog.cpp" line="241"/>
+        <source>Show &amp;details &gt;&gt;</source>
+        <translation>Показать &amp;подробности &gt;&gt;</translation>
+    </message>
+    <message>
+        <location filename="dialog.cpp" line="261"/>
+        <source>Quit</source>
+        <translation>Выход</translation>
+    </message>
 </context>
 <context>
-    <name>MsgDialog</name>
+    <name>Packageinstall</name>
     <message>
-        <location filename="msgdialog.ui" line="14"/>
-        <source>Dialog</source>
-        <translation>Диалог</translation>
+        <location filename="packageinstall.ui" line="14"/>
+        <source>Installing packages</source>
+        <translation>Установка пакетов</translation>
     </message>
     <message>
-        <location filename="msgdialog.ui" line="49"/>
+        <location filename="packageinstall.ui" line="42"/>
+        <source>&lt;b&gt;Installing packages&lt;/b&gt;&lt;br&gt;Please, wait...</source>
+        <translation>&lt;b&gt;Установка пакетов&lt;/b&gt;&lt;br&gt;Пожалуйста, подождите...</translation>
+    </message>
+    <message>
+        <location filename="packageinstall.ui" line="106"/>
+        <source>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Droid Sans&apos;; font-size:8pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:5px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; color:#000000;&quot;&gt;Processed packages:&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Droid Sans&apos;; font-size:8pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:5px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; color:#000000;&quot;&gt;Обрабатываемые пакеты:&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="packageinstall.ui" line="137"/>
+        <source>Display all installation</source>
+        <translation>Показывать весь процесс установки</translation>
+    </message>
+    <message>
+        <location filename="packageinstall.ui" line="140"/>
         <source>Show &amp;details &gt;&gt;</source>
-        <translation>&amp;Показать подробности &gt;&gt;</translation>
+        <translation>Показать &amp;подробности &gt;&gt;</translation>
     </message>
     <message>
-        <location filename="msgdialog.ui" line="69"/>
+        <location filename="packageinstall.ui" line="160"/>
+        <source>Run installation</source>
+        <translation>Запуск установки</translation>
+    </message>
+    <message>
+        <location filename="packageinstall.ui" line="163"/>
         <source>&amp;Install</source>
         <translation>&amp;Установить</translation>
     </message>
     <message>
-        <location filename="msgdialog.ui" line="76"/>
+        <location filename="packageinstall.ui" line="170"/>
+        <source>Quit from application</source>
+        <translation>Выход из приложения</translation>
+    </message>
+    <message>
+        <location filename="packageinstall.ui" line="173"/>
         <source>&amp;Cancel</source>
-        <translation>&amp;Отменить</translation>
+        <translation>&amp;Отмена</translation>
     </message>
 </context>
 <context>
@@ -224,11 +266,6 @@ Check output log.</source>
         <source>Usage: %1 [option] package...</source>
         <comment>%1 is application name</comment>
         <translation>Использование: %1 [параметр] пакет...</translation>
-    </message>
-    <message>
-        <location filename="dialog.cpp" line="270"/>
-        <source>&amp;Close</source>
-        <translation>&amp;Закрыть</translation>
     </message>
 </context>
 </TS>
